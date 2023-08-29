764e348d3d741cfaf107a29e0f54ccf56f3f58b0
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-29 15:57:47 -0400

    show page created

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -2,4 +2,14 @@ class FarmsController < ApplicationController
   def index
     @farms = Farm.all
   end
+
+  def show
+    @farm = Farm.find(params[:id])
+  end
+
+  private
+
+  def farm_params
+    params.require(:farm).permit(:name, :address, :open, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "farms#index"
-  resources :farms, only: :index
+  resources :farms, only: [:index, :show]
 end
