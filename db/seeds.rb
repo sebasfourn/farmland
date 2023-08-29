1ba4363c9157f043183d18e6ff15af73d0deb20c
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-29 12:40:12 -0400

    seeds for products and farms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Farm.destroy_all
+Product.destroy_all
+
+
+Ail_Sherbrooke = Farm.create!(name: "Ail Sherbrooke", address: "", open: true, rating: 4.5)
+Ferme_d_ORee = Farm.create!(name: "Ferme d'ORée", address: "", open: true, rating: 3.5)
+Sanders_Farm = Farm.create!(name: "Sanders Farm", address: "", open: true, rating: 4.5)
+
+
+tomato = Product.new(name: "tomato", category: "vegetable", unit: "kg", stock: 20, farm: Ail_Sherbrooke)
+tomato.save!
