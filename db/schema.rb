e8dc5ab334dbbcf1d04287ef135f762a5a9520ba
Daphne Staruch <daphstr@gmail.com>
2023-08-31 15:55:08 -0400

    added distance from user to driver

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_154801) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_190522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_154801) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -128,6 +128,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_154801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.boolean "electric", default: false
     t.index ["farm_id"], name: "index_trips_on_farm_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
@@ -146,6 +147,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_154801) do
     t.string "last_name"
     t.boolean "electric_car"
     t.boolean "verified", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
