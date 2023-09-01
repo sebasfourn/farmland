e9ab6caa4e05b21bdcdb3cc7f164c3e14be28426
Joe Roberts <roberts.josephpaul@gmail.com>
2023-09-01 12:51:12 -0400

    made method for radio buttons - to improve style

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,14 +15,11 @@ class OrdersController < ApplicationController
     @order.user = current_user
     if @order.save
       params["order"]["order_products"].each_value do |order_product|
-          new_order_product = OrderProduct.new(quantity: order_product["quantity"], product_id: order_product["product_id"])
-          new_order_product.cost = (new_order_product.product.price*new_order_product.quantity)
-          new_order_product.order = @order
-          # new_order_product.product_id = hash["product_id"]
-          # new_order_product.quantity = hash["quantity"]
-          # new_order_product.trip_id = @order.trip_id
-          new_order_product.save
-          end
+        new_order_product = OrderProduct.new(quantity: order_product["quantity"], product_id: order_product["product_id"])
+        new_order_product.cost = (new_order_product.product.price*new_order_product.quantity)
+        new_order_product.order = @order
+        # TODO: update farm stock
+      end
       redirect_to orders_path
     else
       render "farms/show", status: :unprocessable_entity
