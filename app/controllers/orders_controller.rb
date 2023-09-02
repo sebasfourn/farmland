70f4861396340defb3b500d7be2ac33be882705c
Joe Roberts <roberts.josephpaul@gmail.com>
2023-09-02 13:08:05 -0400

    fixed functionality of order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,21 +22,22 @@ class OrdersController < ApplicationController
     @order.user = current_user#
     if @order.save
       params["order"]["order_products"].each_value do |order_product|
-        new_order_product = OrderProduct.new(quantity: order_product["quantity"], product_id: order_product["product_id"])
-        new_order_product.cost = (new_order_product.product.price*new_order_product.quantity)
-        new_order_product.order = @order
-        farm_product = Product.find(new_order_product.product_id)
-        farm_product.stock -= order_product["quantity"].to_i
-        farm_product.save
-        new_order_product.save
-        # TODO: update farm stock
+        if order_product["quantity"].to_i.positive?
+          new_order_product = OrderProduct.new(quantity: order_product["quantity"], product_id: order_product["product_id"])
+          new_order_product.cost = (new_order_product.product.price*new_order_product.quantity)
+          new_order_product.order = @order
+          farm_product = Product.find(new_order_product.product_id)
+          farm_product.stock -= order_product["quantity"].to_i
+          farm_product.save
+          new_order_product.save
+        end
       end
       redirect_to orders_path
     else
       # redirect_to farm_path(params["farm"])
       @farm = Farm.find(params["farm"])
       @products = @farm.products
-      @order = 
+      @order =
       render "farms/show", status: :unprocessable_entity
     end
   end
