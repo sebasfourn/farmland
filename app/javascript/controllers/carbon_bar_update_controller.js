903560cc967b941b1648f4fb25e386784ec5fe70
Joe Roberts <roberts.josephpaul@gmail.com>
2023-09-04 12:18:21 -0400

    pushing unfinished work to save progress

--- app/javascript/controllers/carbon_bar_update_controller.js
+++ app/javascript/controllers/carbon_bar_update_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="carbon-bar-update"
 export default class extends Controller {
+
+  static values = {
+    currentCo2: Number
+  }
+
   connect() {
     console.log("Hello from carbon bar updater")
+  }
+
+  quantity() {
+    console.log(this.currentCo2Value)
   }
 }
