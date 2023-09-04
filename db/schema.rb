080ed823ad1847e9440b8055900ceec5a0a4fb75
Sebastien Fournier <sfournier.qc@gmail.com>
2023-09-04 12:01:18 -0400

    grass fed tag added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_190522) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_153348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_190522) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "grass_fed", default: false
   end
 
   create_table "favorites", force: :cascade do |t|
