613814ece8385911d5de68958e043e779368b9be
Daphne Staruch <daphstr@gmail.com>
2023-09-04 16:56:26 -0400

    carpool text colors

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -9,9 +9,9 @@ class Trip < ApplicationRecord
               <h3>#{self.date.strftime('%B %d %l:%M %p')}</h3>
               <div class='carpool-info'>
                <p>
-                <span class='black-text'>#{self.user.electric_car ? "Electric" : "Gas"}</span> car,
-                <span class='black-text'>#{seat} #{'seat'.pluralize(seat)}</span> available </br>
-                Driver : <span class='black-text'>#{self.user.first_name.capitalize}</span>
+                <span class='emph-carpool'>#{self.user.electric_car ? "Electric" : "Gas"}</span> car,
+                <span class='emph-carpool'>#{seat} #{'seat'.pluralize(seat)}</span> available </br>
+                Driver : <span class='emph-carpool'>#{self.user.first_name.capitalize}</span>
               </p>
               </div>
             </div>".html_safe
