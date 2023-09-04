8ca0020afe8f40a8892d1853c715d4afee7abac1
Joe Roberts <roberts.josephpaul@gmail.com>
2023-09-04 15:19:20 -0400

    added JS so carbon banner updates live

--- app/javascript/controllers/carbon_bar_update_controller.js
+++ app/javascript/controllers/carbon_bar_update_controller.js
@@ -3,15 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="carbon-bar-update"
 export default class extends Controller {
 
-  static values = {
-    currentCo2: Number
-  }
+  static values = { current: Number }
+  static targets = ["quantity", "co2"]
 
-  connect() {
-    console.log("Hello from carbon bar updater")
-  }
+  addProductCo2() {
 
-  quantity() {
-    console.log(this.currentCo2Value)
+    let total = 0
+    document.querySelectorAll('[data-co2-saved]').forEach(s =>
+      total += s.value * s.dataset.co2Saved)
+    const topBanner = document.getElementById("current-user-co2-saved")
+    topBanner.innerHTML = `${(total + this.currentValue).toFixed(2) } kg of CO2!`
   }
 }
