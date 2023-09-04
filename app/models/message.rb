58836c477eeea70f55e3d6de23178b3269ff0ba3
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-09-04 12:28:05 -0400

    progress on chat styling

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :trip
   belongs_to :user
+
+  def sender?(user)
+    if self.user == user
+      return true
+    else
+      return false
+    end
+  end
 end
