fec8b4e16d6fb0e07a2e77b71407a20758b325cf
Joe Roberts <roberts.josephpaul@gmail.com>
2023-09-06 13:48:03 -0400

    update co2 equivalency live

--- app/javascript/controllers/carbon_bar_update_controller.js
+++ app/javascript/controllers/carbon_bar_update_controller.js
@@ -9,12 +9,14 @@ export default class extends Controller {
     const topBannerText = document.getElementById("top-banner-text");
     const topBannerCarbonValue = document.getElementById("current-user-co2-saved");
     const tripCo2 = document.getElementById("trip-co2").innerText
+    const topBannerCo2Equivalent = document.getElementById("co2-equivalent-trees");
     topBannerText.classList.remove("add-carbon-saving");
     let total = 0;
-    document.querySelectorAll('[data-co2-saved]').forEach(s =>
-      total += s.value * s.dataset.co2Saved);
+    document.querySelectorAll('[data-co2-saved]').forEach((s =>
+      total += s.value * s.dataset.co2Saved))
     total += (parseFloat(tripCo2)/1000)
-    topBannerCarbonValue.innerHTML = `${(total + this.currentValue).toFixed(2) } kg of CO2!`;
+    topBannerCarbonValue.innerHTML = `${(total + this.currentValue ).toFixed(2) } kg of CO2!`;
+    topBannerCo2Equivalent.innerText = `${(total + this.currentValue/0.362).toFixed(0)} weeks`
     topBannerText.classList.add("add-carbon-saving");
     setTimeout(function() {
       topBannerText.classList.remove("add-carbon-saving");
