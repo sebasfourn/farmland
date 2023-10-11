a1cec8e89e6a46e13d9835835a8517759d8152f6
sebastien fournier <sfournier.qc@gmail.com>
2023-10-11 12:28:23 -0400

    adding sign out link on address

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
+
   resources :farms, only: %i[index show] do
     member do
       post :favorite
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   resources :orders, only: %i[index show create update] do
     resources :order_products, only: %i[create]
   end
+
   resources :trips, only: %i[index show create update] do
     resources :messages, only: :create
   end
